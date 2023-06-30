2c7abd1251d9925f35ab8040aaa04998f99ef101
georgeschaaya <138156243+georgeschaaya@users.noreply.github.com>
2023-06-30 13:40:14 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # Georges Chaaya
+   # georgeschaaya
    # Input:
    # p, principal amount
    # t, time period in years
